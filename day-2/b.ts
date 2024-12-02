dce0f0788c2ce7c881b4fa7ab2d4f9ccb67875f8
Illia Abashkin <abashkinis@gmail.com>
2024-12-02 16:33:30 +0000

    2-b

--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,47 @@
+import { log } from 'console';
 import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+  return data.map(isSafeByRemovingOneLevel).filter((isSafe) => isSafe).length;
 }
 
 await runSolution(day2b);
+
+function isSafeByRemovingOneLevel(row: string): boolean {
+  const rowNums = row.split(' ').map(parseFloat);
+  for (let indx = 0; indx < rowNums.length; indx++) {
+    const tmpNums: number[] = rowNums
+      .slice(0, indx)
+      .concat(rowNums.slice(indx + 1));
+    if (isSafeRow(tmpNums)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isSafeRow(nums: number[]): boolean {
+  let isIncreasing: boolean | null = null;
+
+  for (let index = 0; index < nums.length - 1; index++) {
+    const num1 = nums.at(index);
+    const num2 = nums.at(index + 1);
+    const delta = num2 - num1;
+
+    if (delta > 0) {
+      if (isIncreasing === false) return false;
+      isIncreasing = true;
+    }
+
+    if (delta < 0) {
+      if (isIncreasing === true) return false;
+      isIncreasing = false;
+    }
+
+    if (Math.abs(delta) > 3 || Math.abs(delta) < 1) {
+      return false;
+    }
+  }
+  return true;
+}
