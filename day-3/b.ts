550a79e7bcdadca9c514617cc0ab246c7cb98e84
Illia Abashkin <142309158+IlliaAbashkinTide@users.noreply.github.com>
2024-12-03 22:06:30 +0200

    3-b working

--- day-3/b.ts
+++ day-3/b.ts
@@ -2,23 +2,16 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
-  const fromDontToDo = /don\'t\(\)(.*?)do\(\)/g;
+  const fromDoToDont = /do\(\).*?don't\(\)/g;
   const mulMatcher = /mul\(\d{1,3},\d{1,3}\)/g;
-  const firstMulMatcher = /mul\(\d{1,3},\d{1,3}\)/;
 
-  //data.join().match(fromDoToDont).forEach(console.log);
-  console.log(data.join().replace(fromDontToDo, 'do()'));
-
-  const doSum = data
-    .join()
-    .replace(fromDontToDo, '')
+  return ['do()', ...data, 'dont()'].join()
+    .match(fromDoToDont).join()
     .match(mulMatcher)
     .reduce((prev, curr) => {
       const result = excecuteMul(curr);
       return prev + result;
     }, 0);
-  const firstMul = data[0].match(firstMulMatcher).join();
-  return doSum + excecuteMul(firstMul);
 }
 
 function excecuteMul(mulStr: string): number {
