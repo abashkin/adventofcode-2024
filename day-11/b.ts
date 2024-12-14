c37cdd6cac01aa4efae9faba9548a01d198104c5
Illia Abashkin <142309158+IlliaAbashkinTide@users.noreply.github.com>
2024-12-14 19:13:01 +0200

    11-b

--- day-11/b.ts
+++ day-11/b.ts
@@ -1,44 +1,34 @@
-import { count } from 'console';
 import { runSolution } from '../utils.ts';
-
-class Node {
-  public value: number;
-  public children: Node[];
-  constructor(value) {
-    this.value = value;
-    this.children = [];
-  }
-}
-
-class Counter {
-  private count: number;
-  constructor() {
-    this.count = 0;
-  }
-
-  add(): void {
-    this.count++;
-  }
-
-  total(): number {
-    return this.count;
-  }
-}
 
 /** provide your solution as the return of this function */
 export async function day11b(data: string[]) {
   console.log(data);
   const blinkingTimes = 75;
-  const chunkSize = 25;
   const initialArrangement = data[0].split(' ').map(Number);
-  const counter = new Counter();
+  let stones: Map<number, number> = new Map();
+  initialArrangement.forEach(stone => stones.set(stone, 1));
 
-  initialArrangement.forEach((stone: Number) => {
-    const root = new Node(stone);
-    buildStoneTransformationTree(root, blinkingTimes, transformStone, counter);
-  });
+  for(let i = 0; i< blinkingTimes; i++) {
+    const blinkMap: Map<number, number> = new Map();
+    for (const stone of stones.keys()) {
+      const numOfStones = stones.get(stone);
+      for (const newStone of transformStone(stone)) {
+        if(!blinkMap.has(newStone)) {
+          blinkMap.set(newStone, numOfStones)
+        } else {
+          blinkMap.set(newStone, blinkMap.get(newStone) + numOfStones)
+        }
+      }
+    }
+    stones = blinkMap;
+  }
+  let result = 0;
+  for (const stone of stones.keys()) {
+    result +=stones.get(stone)
+  }
 
-  console.log('Total: ', counter.total());
+  console.log(result);
+
   return 0;
 }
 
@@ -60,61 +50,3 @@ function splitStone(stone: number): number[] {
   return [Number(s1), Number(s2)];
 }
 
-function buildStoneTransformationTree(
-  node: Node,
-  times: number,
-  transformationLogic: (number) => number[],
-  counter: Counter
-) {
-  if (times === 0) {
-    console.log(node.value);
-    counter.add();
-    return;
-  }
-  const stepStones = transformationLogic(node.value);
-  for (const stone of stepStones) {
-    const childNode = new Node(stone);
-    node.children.push(childNode);
-    buildStoneTransformationTree(
-      childNode,
-      times - 1,
-      transformationLogic,
-      counter
-    );
-  }
-}
-
-function chunkedComputation(
-  initialNodes: number[],
-  totalSteps: number,
-  chunkSize: number,
-  transformationLogic: (number) => number[]
-): number {
-  let currentNodes = initialNodes.map((n) => new Node(n));
-  let remainingSteps = totalSteps;
-
-  while (remainingSteps > 0) {
-    const nextNodes: number[] = [];
-
-    for (const node of currentNodes) {
-      nextNodes.push(...transformationLogic(node.value));
-    }
-
-    if (nextNodes.length > chunkSize) {
-      let newLeaves = 0;
-      for (const leaf of nextNodes) {
-        newLeaves += chunkedComputation(
-          [leaf],
-          remainingSteps - 1,
-          chunkSize,
-          transformationLogic
-        );
-      }
-
-      return newLeaves;
-    }
-
-    currentNodes = nextNodes.map((n) => new Node(n));
-    remainingSteps -= 1;
-  }
-}
