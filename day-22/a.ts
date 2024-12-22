f8ea42e35e037148151a1c4cd8edfd465742ef23
Illia Abashkin <abashkinis@gmail.com>
2024-12-22 18:43:01 +0000

    22-a

--- day-22/a.ts
+++ day-22/a.ts
@@ -3,7 +3,32 @@ import { runSolution } from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day22a(data: string[]) {
   console.log(data);
+
+  let total = 0n;
+  data.forEach((sec) => {
+    let s = BigInt(sec);
+    for (let i = 0; i < 2000; i++) {
+      s = (round(s << 6n) ^ s) & 0xffffffn;
+      s = ((s >> 5n) ^ s) & 0xffffffn;
+      s = ((s << 11n) ^ s) & 0xffffffn;
+    }
+    total += s;
+  });
+
+  console.log(total);
   return 0;
 }
 
 await runSolution(day22a);
+
+function mix(num: bigint, s: bigint): bigint {
+  return num ^ s;
+}
+
+function prune(s: bigint): bigint {
+  return s % 16777216n;
+}
+
+function round(n: bigint): bigint {
+  return BigInt(`${n}`.split('.')[0]);
+}
